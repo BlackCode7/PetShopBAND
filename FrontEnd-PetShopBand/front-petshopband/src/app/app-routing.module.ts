960a28147195a71d0c0bcc4780109642b90dba69
Anderson <BlackCode7@outlook.com>
2022-03-03 06:23:57 -0300

    Adicionando as rotas no app routing

--- FrontEnd-PetShopBand/front-petshopband/src/app/app-routing.module.ts
+++ FrontEnd-PetShopBand/front-petshopband/src/app/app-routing.module.ts
@@ -4,20 +4,23 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 
-const routes: Routes = [{
-  path: '',
-  component: AppComponent,
-  children: [
-    {
-      path: 'login',
-      loadChildren: () => import('./auth/login/login.module').then(m => m.LoginModule)
-    },
-    {
-      path: 'register',
-      loadChildren: () => import('./auth/register/register.module').then(m => m.RegisterModule)
-    }
-  ]
-}];
+const routes: Routes = [
+  {path: '', component: AppComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent}
+];
+  
+  // children: [
+  //   {
+  //     path: 'login',
+  //     loadChildren: () => import('./auth/login/login.module').then(m => m.LoginModule)
+  //   },
+  //   {
+  //     path: 'register',
+  //     loadChildren: () => import('./auth/register/register.module').then(m => m.RegisterModule)
+  //   }
+  // ]
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
